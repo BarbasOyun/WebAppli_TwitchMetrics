efdd6d76a1a158ea2c4ae549da1a60ceb8c5b842
Barbas_Oyun <yoan.lechevalier@gmail.com>
2021-03-23 16:23:18 +0100

    View Sphere

--- WebAppli_Twitch/script.js
+++ WebAppli_Twitch/script.js
@@ -8,7 +8,7 @@ document.body.appendChild( renderer.domElement )
 var geometry = new THREE.BoxGeometry( 1, 1, 1)
 var material = new THREE.MeshStandardMaterial( { color: 0xff0051 })
 var cube = new THREE.Mesh ( geometry, material )
-scene.add( cube )
+//scene.add( cube )
 
 var geometry_01 = new THREE.BoxGeometry( 50, 50, 50)
 var cube_01 = new THREE.Mesh ( geometry_01, material )
@@ -17,10 +17,14 @@ cube_01.position.x = 20;
 cube_01.position.y = 0;
 cube_01.position.z = -50;
 
-const geometrySphere = new THREE.SphereGeometry( 5, 32, 32 );
-const materialSphere = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-const sphere = new THREE.Mesh( geometrySphere, materialSphere );
+var geometrySphere = new THREE.SphereGeometry( 1, 32, 32 );
+var materialSphere = new THREE.MeshStandardMaterial( {color: 0xffff00} );
+var sphere = new THREE.Mesh( geometrySphere, materialSphere );
 scene.add( sphere );
+sphere.position.x = 0 
+sphere.position.y = 0 
+sphere.position.z = -5
+
 
 camera.position.z = 5
 
@@ -32,6 +36,13 @@ pointLight.position.set( 25, 50, 25 );
 scene.add( pointLight );
 
 renderer.render( scene, camera )
+
+function CreateSphere(radius)
+{
+    var geometrySphere_01 = new THREE.SphereGeometry( radius, 32, 32 );
+    var sphere_01 = new THREE.Mesh( geometrySphere_01, materialSphere );
+    scene.add( sphere_01 );
+}
 
 function animate() {
     requestAnimationFrame( animate )
