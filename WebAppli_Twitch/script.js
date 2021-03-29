4eb017ca2eab73723d30a64356be5ec6bd2fe18b
Barbas_Oyun <yoan.lechevalier@gmail.com>
2021-03-29 10:50:48 +0200

    Update

--- WebAppli_Twitch/script.js
+++ WebAppli_Twitch/script.js
@@ -5,19 +5,34 @@ const renderer = new THREE.WebGLRenderer({ antialias: true})
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
 
-var geometry_01 = new THREE.BoxGeometry( 50, 50, 50)
+var raycaster, mouse = { x : 0, y : 0 };
+
+/*var geometry_01 = new THREE.BoxGeometry( 50, 50, 50)
 var material = new THREE.MeshStandardMaterial( { color: 0xff0051 })
 var cube_01 = new THREE.Mesh ( geometry_01, material )
 scene.add( cube_01 )
 cube_01.position.x = 20;
 cube_01.position.y = 0;
-cube_01.position.z = -50;
+cube_01.position.z = -50;*/
+
+var font_01 = new THREE.FontLoader();
+font_01.load("https://github.com/chalupagrande/threejs-text-example/blob/f07f25c31f074f0fdbb71d83124bab1b74930280/mk3_regular.js", function (res){
+    font = res;
+
+    text_01 = new THREE.TextGeometry( "text",  {
+        font: font,
+        size: size,
+        height: height,
+    });
+
+    scene.add(text_01);
+});
+
 
 const spheres = [CreateSphere(3, 0xffff00, -5), CreateSphere(1, 0xffff00, 0), CreateSphere(2, 0xffff00, 5)];
 spheres.forEach((sphere, ndx) => {
     scene.add( sphere );
 });
-
 
 camera.position.z = 5
 
@@ -40,11 +55,17 @@ function CreateSphere(radius, color, x)
     return sphere_01;
 }
 
+init();
+
+function init () {
+    raycaster = new THREE.Raycaster();
+}
+
 function animate() {
     requestAnimationFrame( animate )
 
-    cube_01.rotation.x += 0.001;
-    cube_01.rotation.y += 0.001;
+    /*cube_01.rotation.x += 0.001;
+    cube_01.rotation.y += 0.001;*/
 
     spheres.forEach((sphere, ndx) => {
         sphere.position.x += 0.1 * ndx;
@@ -53,3 +74,34 @@ function animate() {
     renderer.render( scene, camera )
 }
 animate()
+
+document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+
+function onDocumentMouseDown( event ) {
+
+    event.preventDefault();
+
+     //1. sets the mouse position with a coordinate system where the center
+    //   of the screen is the origin
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    //2. set the picking ray from the camera position and mouse coordinates
+    raycaster.setFromCamera( mouse, camera );    
+
+    //3. compute intersections
+    var intersects = raycaster.intersectObjects( scene.children );
+
+    for ( var i = 0; i < intersects.length; i++ ) {
+        console.log( intersects[ i ] ); 
+        /*
+            An intersection has the following properties :
+                - object : intersected object (THREE.Mesh)
+                - distance : distance from camera to intersection (number)
+                - face : intersected face (THREE.Face3)
+                - faceIndex : intersected face index (number)
+                - point : intersection point (THREE.Vector3)
+                - uv : intersection point in the object's UV coordinates (THREE.Vector2)
+        */
+    }
+}
